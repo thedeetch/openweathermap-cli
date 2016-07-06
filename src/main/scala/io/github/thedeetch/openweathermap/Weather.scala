d5f2b9aa7ff6776928f30ed0f31f1ad1285fe4b3
Nick Dietrich <nicholas.dietrich@gmail.com>
2016-07-05 22:55:30 -0500

    Update docs on OpenWeatherMap classes.

--- src/main/scala/io/github/thedeetch/openweathermap/Weather.scala
+++ src/main/scala/io/github/thedeetch/openweathermap/Weather.scala
@@ -1,12 +1,22 @@
 package io.github.thedeetch.openweathermap
 
 /**
-  * Created by nick on 7/5/16.
+  * Corresponds to OpenWeatherMap current weather API response.
+  * @param id the city ID
+  * @param name the city name
+  * @param main the main weather conditions
+  * @param sys additional information regarding the location
   */
 case class WeatherResponse(id: Long, name: String, main: Main, sys: Sys)
 
+/**
+  * Main weather conditions.
+  * @param temp the current temperature
+  */
 case class Main(temp: Double)
 
+/**
+  * Information regarding the location.
+  * @param country the country where the location is located
+  */
 case class Sys(country: String)
-
-
